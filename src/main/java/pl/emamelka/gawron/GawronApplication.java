2db895fcef6e90132d3d2b9610cbd17e927b7938
emamelka <edyta.mamelka1@gmail.com>
2019-01-27 12:26:42 +0100

    Initial commit

---
+++ src/main/java/pl/emamelka/gawron/GawronApplication.java
@@ -0,0 +1,14 @@
+package pl.emamelka.gawron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GawronApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GawronApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/pl/emamelka/gawron/GawronApplicationTests.java
@@ -0,0 +1,17 @@
+package pl.emamelka.gawron;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GawronApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
